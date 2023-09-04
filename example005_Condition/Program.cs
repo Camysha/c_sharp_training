11abc9ffdde9237f1115607d766f033eed10a52f
Alexandra <alexandra-sh@mai.ru>
2023-09-04 07:26:12 +0300

    exmple005 condition

---
+++ example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Приввет, ");
+    Console.WriteLine(username);
+}
